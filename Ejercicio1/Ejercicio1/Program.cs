d6b589b6d01eeb5b6636fa29d10c40d8c311f825
William Torres <ISNP111124@ugb.edu.sv>
2024-10-20 23:24:58 -0600

    Ejercicio1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.Write("Ingrese un número para determinar si es positivo o negativo, presione 0 para salir: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num > 0)
+                {
+                    Console.WriteLine("El número {0} es positivo.", num);
+                }
+                else if (num < 0)
+                {
+                    Console.WriteLine("El número {0} es negativo.", num);
+                }
+
+            } while (num != 0);
+        }
+    }
+}
