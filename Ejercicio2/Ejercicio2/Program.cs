1b7276a0650dcc1588fedae177e281b5e9e727f2
William Torres <ISNP111124@ugb.edu.sv>
2024-10-20 23:27:57 -0600

    Ejercicio2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            do
+            {
+                Console.Write("Ingrese un número entre 1 y 10 para ver su tabla de multiplicar (0 para salir): ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num >= 1 && num <= 10)
+                {
+                    Console.WriteLine($"\nTabla de multiplicar del {num}:");
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        Console.WriteLine($"{num} x {i} = {num * i}");
+                    }
+                }
+                else if (num != 0)
+                {
+                    Console.WriteLine("Número fuera del rango. Intente de nuevo.");
+                }
+
+                Console.WriteLine(); // Línea en blanco para mejor legibilidad
+            } while (num != 0);
+        }
+    }
+}
